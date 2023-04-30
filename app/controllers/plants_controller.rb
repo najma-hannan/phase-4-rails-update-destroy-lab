5c53a962d98690d31a42f4b483cb1b014b2e31f6
Hannan <najma.mahat@student.moringaschool.com>
2023-04-30 11:25:26 +0300

    Created delete route

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -18,9 +19,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :ok
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
   end
 end
